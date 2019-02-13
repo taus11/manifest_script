20ed82a53ebbf9d25082527f5577036b25adaae1
tausif <msiddiqu@redhat.com>
2019-02-13 20:20:50 +0530

    Change repo path.

--- manifest.sh
+++ manifest.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# containers_dir = ~/git/gitlab/manifests/containers/
+# manifest_dir = ~/git/gitlab/manifests/*
+
 function manifest_search {
 
     printf "\n\033[1;31m ====================== START =========================\033[0m\n"
@@ -9,35 +12,35 @@ function manifest_search {
 
     if [ $# == 1 ]; then
           printf "\n\033[1;31m 1st Filter \033[0m\n"
-          egrep --exclude-dir=/home/msiddiqu/prodsec/documentation/process-docs/manifest/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" /home/msiddiqu/prodsec/documentation/process-docs/manifest/*
+          egrep --exclude-dir=~/git/gitlab/manifests/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" ~/git/gitlab/manifests/*
     fi
 
     if [ $# == 2 ]; then
           printf "\n\033[1;31m 2nd Filter\033[0m\n"
-          egrep --exclude-dir=/home/msiddiqu/prodsec/documentation/process-docs/manifest/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" /home/msiddiqu/prodsec/documentation/process-docs/manifest/* | egrep -Evi -e "$2" | uniq --group -w 9 | egrep -zi "$1"
+          egrep --exclude-dir=~/git/gitlab/manifests/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" ~/git/gitlab/manifests/* | egrep -Evi -e "$2" | uniq --group -w 9 | egrep -zi "$1"
     fi
 
     if [ $# == 3 ]; then
           printf "\n\033[1;31m 3rd Filter\033[0m\n"
-          egrep --exclude-dir=/home/msiddiqu/prodsec/documentation/process-docs/manifest/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" /home/msiddiqu/prodsec/documentation/process-docs/manifest/* | egrep -Evi -e "$2" | egrep -Evi -e "$3" | uniq --group -w 9 | egrep -zi "$1"
+          egrep --exclude-dir=~/git/gitlab/manifests/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" ~/git/gitlab/manifests/* | egrep -Evi -e "$2" | egrep -Evi -e "$3" | uniq --group -w 9 | egrep -zi "$1"
 
     fi
 
     if [ $# == 4 ]; then
           printf "\n\033[1;31m 4th Filter\033[0m\n"
-          egrep --exclude-dir=/home/msiddiqu/prodsec/documentation/process-docs/manifest/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" /home/msiddiqu/prodsec/documentation/process-docs/manifest/* | egrep -Evi -e "$2" | egrep -Evi -e "$3" | egrep -Evi -e "$4" | uniq --group -w 9 | egrep -zi "$1"
+          egrep --exclude-dir=~/git/gitlab/manifests/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" ~/git/gitlab/manifests/* | egrep -Evi -e "$2" | egrep -Evi -e "$3" | egrep -Evi -e "$4" | uniq --group -w 9 | egrep -zi "$1"
 
     fi
 
     if [ $# == 5 ]; then
           printf "\n\033[1;31m 5th Filter \033[0m\n"
-          egrep --exclude-dir=/home/msiddiqu/prodsec/documentation/process-docs/manifest/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" /home/msiddiqu/prodsec/documentation/process-docs/manifest/* | egrep -Evi -e "$2" | egrep -Evi -e "$3" | egrep -Evi -e "$4" | egrep -Evi -e "$5" | uniq --group -w 9 | egrep -zi "$1"
+          egrep --exclude-dir=~/git/gitlab/manifests/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" ~/git/gitlab/manifests/* | egrep -Evi -e "$2" | egrep -Evi -e "$3" | egrep -Evi -e "$4" | egrep -Evi -e "$5" | uniq --group -w 9 | egrep -zi "$1"
 
     fi
 
     if [ $# == 6 ]; then
           printf "\n\033[1;31m 6th Filter, till here I'm letting it supported in this script \033[0m\n"
-          egrep --exclude-dir=/home/msiddiqu/prodsec/documentation/process-docs/manifest/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" /home/msiddiqu/prodsec/documentation/process-docs/manifest/* | egrep -Evi -e "$2" | egrep -Evi -e "$3" | egrep -Evi -e "$4" | egrep -Evi -e "$5" | egrep -Evi -e "$6" | uniq --group -w 9 | egrep -zi "$1"
+          egrep --exclude-dir=~/git/gitlab/manifests/containers --exclude='manifest-eol.txt' -r -h -i -e "$1" ~/git/gitlab/manifests/* | egrep -Evi -e "$2" | egrep -Evi -e "$3" | egrep -Evi -e "$4" | egrep -Evi -e "$5" | egrep -Evi -e "$6" | uniq --group -w 9 | egrep -zi "$1"
 
     fi
     echo
