ee8895158387035563c87383a02a8870348762cc
tausif <msiddiqu@redhat.com>
2019-06-08 01:54:28 +0530

    Added instructions to find and replace directories

--- manifest.sh
+++ manifest.sh
@@ -1,7 +1,9 @@
+#Instructions:
+#Change just the two manifest_dir and containers_dir with the path in your file system exactly as its written here. I suggest using find and replace.
+#NOTE: Please use full path name and not ~ for your home dir because it matches the output that has full path.
+
 manifest_dir=/home/msiddiqu/git/gitlab/manifests/*
 containers_dir=/home/msiddiqu/git/gitlab/manifests/containers
-
-#NOTE: Please use full path name and not ~ for your home dir because it matches the output that has full path.
 
 function manifest_search {
 
